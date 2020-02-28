db01405d71926d16e18db9dafac5f263555e1e30
Kholkin <kholkins@mail.ru>
2020-02-28 17:34:45 +0300

    ListV

--- app/src/main/java/com/example/andro3/activity/MainActivity.java
+++ app/src/main/java/com/example/andro3/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.andro3;
+package com.example.andro3.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.example.andro3.R;
 
 import java.util.ArrayList;
 import java.util.List;
---
+++ app/src/main/java/com/example/andro3/pojo/PhoneModel.java
@@ -0,0 +1,14 @@
+package com.example.andro3.pojo;
+
+public class PhoneModel {
+
+    private long id;
+    private String name;
+    private long price;
+
+    public PhoneModel(long id, String name, long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
